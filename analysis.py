76d13aeb8f9f59928dd2ddbc3dec31c77409fdd6
Swati Singh <swati2111010@akgec.ac.in>
2025-11-03 08:55:34 +0530

    Created a column age-group

--- analysis.py
+++ analysis.py
@@ -13,4 +13,8 @@ print(df.isnull().sum())
 df.columns = df.columns.str.lower()
 df.columns = df.columns.str.replace(' ', '_')
 df = df.rename(columns={'purchase_amount_(usd)': 'purchase_amount'})
-print(df.columns)
+df.columns
+
+labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
+df['age-group'] = pd.qcut(df['age'], q=4, labels = labels)
+print(df[['age', 'age-group']].head(10))
